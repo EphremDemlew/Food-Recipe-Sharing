0e1b2b1bc28fe6fa70343f1ecdc61c47af7c8889
Ephrem Demlew <ephyaabb@gmail.com>
2022-08-26 18:32:25 +0300

    deleted some unused code

--- express/queries/paymentDetails.js
+++
@@ -1,9 +0,0 @@
-const paymentDetails = `mutation paymentDetail($status:String! , $order_id:uuid!) {
-  insert_payment_details(objects: {status: $status, order_id: $order_id}) {
-    returning {
-      id
-    }
-  }
-}
-`;
-module.exports = paymentDetails;
